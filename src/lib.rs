fb54263745a654934e317779ed518775313c1c07
Delma <delma@del.ma>
2019-11-23 03:18:08 +0200

    Example parses

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,46 @@
 use nom::branch::*;
 use nom::bytes::complete::*;
 use nom::character::complete::*;
+use nom::sequence::*;
 use nom::combinator::*;
 use nom::multi::*;
-use nom::{error::ErrorKind, IResult};
+use nom::{error::{ParseError, ErrorKind}, IResult, InputLength};
 
 use std::cell::RefCell;
 use std::collections::HashSet;
 
 #[derive(PartialEq, Debug)]
+pub struct Ident(pub String);
+
+#[derive(PartialEq, Debug)]
 pub enum Expr {
-    Op(Op),
+    Scope(Box<Expr>),
+    Op(Box<Expr>, Op, Box<Expr>),
     Conditional {
         condition: Box<Expr>,
         success: Box<Expr>,
-        failure: Option<Box<Expr>>,
+        failure: Box<Option<Expr>>,
     },
+    Definition(Ident, Box<Expr>),
+    Call(Ident, Vec<Expr>),
+    Param(Number),
     Text(String),
-    Number(i64),
+    Number(Number),
     WriteIO(Box<Expr>),
     ReadIO,
 }
 
 #[derive(PartialEq, Debug)]
 pub enum Op {
-    Add(Box<Expr>, Box<Expr>),
-    Mul(Box<Expr>, Box<Expr>),
-    Sub(Box<Expr>, Box<Expr>),
+    Equ,
+    Add,
+    Mul,
+    Sub,
 }
 
-pub const ifs: &[&str] = &[
+type Number = u64;
+
+pub const IFS: &[&str] = &[
     "given that",
     "assuming that",
     "conceding that",
@@ -43,7 +54,7 @@ pub const ifs: &[&str] = &[
     "with the condition that",
 ];
 
-pub const elses: &[&str] = &[
+pub const ELSES: &[&str] = &[
     "otherwise",
     "differently",
     "any other way",
@@ -62,6 +73,7 @@ pub const elses: &[&str] = &[
 pub struct State {
     used_conditionals: RefCell<HashSet<usize>>,
     used_elses: RefCell<HashSet<usize>>,
+    level: u64,
 }
 
 impl Default for State {
@@ -69,19 +81,25 @@ impl Default for State {
         State {
             used_conditionals: RefCell::new(HashSet::new()),
             used_elses: RefCell::new(HashSet::new()),
+            level: 0,
         }
     }
 }
 
 //🆘
 
-pub fn ws<T>(parser: impl Fn(&str) -> IResult<&str, T>) -> impl Fn(&str) -> IResult<&str, T> {
-    move |code| {
-        let (code, res) = parser(code)?;
-        let (code, _) = space0(code)?;
-        Ok((code, res))
+pub fn ws<'a, T: 'a>(parser: impl Fn(&'a str) -> IResult<&'a str, T>) -> impl Fn(&'a str) -> IResult<&'a str, T> {
+    terminated(parser, space0)
+}
+
+pub fn eof<I: Copy + InputLength, E: ParseError<I>>(input: I) -> IResult<I, I, E> {
+    if input.input_len() == 0 {
+        Ok((input, input))
+    } else {
+        Err(nom::Err::Error(E::from_error_kind(input, ErrorKind::Eof)))
     }
 }
+
 
 pub fn cond<'a>(
     used: &'a RefCell<HashSet<usize>>,
@@ -105,43 +123,149 @@ pub fn cond<'a>(
     }
 }
 
-pub fn otherwise<'a>(state: &'a State) -> impl Fn(&str) -> IResult<&str, Box<Expr>> + 'a {
+pub fn otherwise<'a>(state: &'a State) -> impl Fn(&str) -> IResult<&str, Expr> + 'a {
     move |code| {
-        let (code, _) = cond(&state.used_elses, &elses)(code)?;
-        let (code, failure) = expr(state, code)?;
+        let (code, _) = ws(cond(&state.used_elses, &ELSES))(code)?;
+        let (code, failure) = expr(state)(code)?;
         Ok((code, failure))
     }
 }
 
 pub fn paren_start(code: &str) -> IResult<&str, &str> {
-    tag("{")(code)
+    ws(tag("{"))(code)
+}
+
+pub fn paren_end(code: &str) -> IResult<&str, &str> {
+    ws(tag(")"))(code)
 }
 
 pub fn conditional<'a>(state: &'a State) -> impl Fn(&str) -> IResult<&str, Expr> + 'a {
     move |code| {
-        let (code, _) = ws(cond(&state.used_conditionals, ifs))(code)?;
-        let (code, condition) = expr(state, code)?;
-        let (code, _) = paren_start(code)?;
-        let (code, success) = expr(state, code)?;
+        let (code, _) = ws(cond(&state.used_conditionals, IFS))(code)?;
+        let (code, condition) = expr(state)(code)?;
+        let (code, success) = expr(state)(code)?;
         let (code, failure) = opt(otherwise(state))(code)?;
         Ok((
             code,
             Expr::Conditional {
-                condition,
-                success,
-                failure,
+                condition: Box::new(condition),
+                success: Box::new(success),
+                failure: Box::new(failure),
             },
         ))
     }
 }
 
-pub fn number(code: &str) -> IResult<&str, Expr> {
+pub fn number(code: &str) -> IResult<&str, Number> {
     let (code, s) = alt((tag("."), tag(":")))(code)?;
     let n = if s == "." { 1 } else { 2 };
     let (code, m) = many0_count(tag(":"))(code)?;
-    Ok((code, Expr::Number(n + 2 * m as i64)))
+    Ok((code, n + 2 * m as Number))
 }
 
-pub fn expr<'a>(state: &State, code: &'a str) -> IResult<&'a str, Box<Expr>> {
-    map(alt((conditional(state), number)), Box::new)(code)
+pub fn param(code: &str) -> IResult<&str, Expr> {
+    let (code, _) = tag(r"\")(code)?;
+    let (code, n) = number(code)?;
+    Ok((code, Expr::Param(n)))
+}
+
+pub fn ident(code: &str) -> IResult<&str, Ident> {
+    map(
+        take_while1(|c: char| !c.is_ascii() && c.is_alphanumeric()),
+        |s: &str| Ident(s.into())
+    )(code)
+}
+
+pub fn fundef<'a>(state: &'a State) -> impl Fn(&str) -> IResult<&str, Expr> + 'a {
+    move |code| {
+        let (code, name) = ws(ident)(code)?;
+        let (code, _) = ws(tag("¤"))(code)?;
+        let (code, body) = expr(state)(code)?;
+        Ok((code, Expr::Definition(name, Box::new(body))))
+    }
+}
+
+pub fn funcall<'a>(state: &'a State) -> impl Fn(&str) -> IResult<&str, Expr> + 'a {
+    move |code| {
+        let (code, name) = ws(ident)(code)?;
+        let (code, params) = many0(expr(state))(code)?;
+        Ok((code, Expr::Call(name, params)))
+    }
+}
+
+pub fn scope<'a>(state: &'a State) -> impl Fn(&str) -> IResult<&str, Expr> + 'a {
+    move |code| {
+        let (code, _) = paren_start(code)?;
+        let (code, body) = expr(state)(code)?;
+        let (code, _) = alt((
+            paren_end,
+            peek(line_ending),
+            eof
+        ))(code)?;
+        Ok((code, Expr::Scope(Box::new(body))))
+    }
+}
+
+pub fn oper<'a>(state: &'a State) -> impl Fn(&str) -> IResult<&str, (Op, Box<Expr>)> + 'a {
+    move |code| {
+        let (code, op) = ws(alt((
+            map(tag("="), |_| Op::Equ),
+            map(tag("*"), |_| Op::Mul),
+            map(tag("+"), |_| Op::Add),
+            map(tag("-"), |_| Op::Sub)
+        )))(code)?;
+        let (code, rhs) = expr(state)(code)?;
+        Ok((code, (op, Box::new(rhs))))
+    }
+}
+
+pub fn write_io<'a>(state: &'a State) -> impl Fn(&str) -> IResult<&str, Expr> + 'a {
+    move |code| {
+        let (code, _) = ws(tag("@"))(code)?;
+        let (code, _) = ws(tag("<<"))(code)?;
+        let (code, rhs) = expr(state)(code)?;
+        Ok((code, Expr::WriteIO(Box::new(rhs))))
+    }
+}
+
+pub fn text(mut code1: &str) -> IResult<&str, Expr> {
+    let mut s = String::new();
+    loop {
+        let (code, _) = tag("/")(code1)?;
+        let (code, t) = opt(tag("/"))(code)?;
+        if t.is_some() {
+            s.push('/');
+        }
+        let (code, t) = opt(tag(" "))(code)?;
+        if t.is_some() {
+            s.push(' ');
+        }
+        let (code, part) = take_while(|c: char| c != ' ' && c != '/' && c != '\r' && c != '\n')(code)?;
+        s.push_str(part);
+        code1 = code;
+        if code.is_empty() {
+            break;
+        }
+        let c = code.chars().next().unwrap();
+        if c == ' ' || c == '\r' || c == '\n' {
+            break;
+        }
+    }
+    Ok((code1, Expr::Text(s)))
+}
+
+pub fn expr<'a>(state: &'a State) -> impl Fn(&str) -> IResult<&str, Expr> + 'a {
+    move |code| {
+        let (code, expr) = ws(alt((fundef(state), funcall(state), text, write_io(state), scope(state), conditional(state), param, map(number, Expr::Number))))(code)?;
+        let (code, oper) = opt(oper(state))(code)?;
+        if let Some((op, rhs)) = oper {
+            Ok((code, Expr::Op(Box::new(expr), op, rhs)))
+        } else {
+            Ok((code, expr))
+        }
+    }
+}
+
+pub fn parse<'a>(state: &State, code: &'a str) -> IResult<&'a str, Vec<Expr>> {
+    delimited(multispace0, separated_list(multispace1, expr(state)), multispace0)(code)
 }
--- tests/parser.rs
+++ tests/parser.rs
@@ -1,56 +1,375 @@
-use sos::{expr, Expr, State};
+use sos::{parse, Expr, State, Ident, Op};
 
 fn b<T>(t: T) -> Box<T> {
     Box::new(t)
 }
 
-fn r<'a, T>(t: T) -> nom::IResult<&'a str, Box<T>> {
-    Ok(("", b(t)))
+fn r<'a, T>(t: T) -> nom::IResult<&'a str, T> {
+    Ok(("", t))
 }
 
 #[test]
 fn parse_numbers() {
-    assert_eq!(r(Expr::Number(1)), expr(&State::default(), "."));
-    assert_eq!(r(Expr::Number(2)), expr(&State::default(), ":"));
-    assert_eq!(r(Expr::Number(3)), expr(&State::default(), ".:"));
-    assert_eq!(r(Expr::Number(4)), expr(&State::default(), "::"));
-    assert_eq!(r(Expr::Number(5)), expr(&State::default(), ".::"));
-    assert_eq!(r(Expr::Number(6)), expr(&State::default(), ":::"));
-    assert_eq!(r(Expr::Number(7)), expr(&State::default(), ".:::"));
+    assert_eq!(r(vec![Expr::Number(1)]), parse(&State::default(), "."));
+    assert_eq!(r(vec![Expr::Number(2)]), parse(&State::default(), ":"));
+    assert_eq!(r(vec![Expr::Number(3)]), parse(&State::default(), ".:"));
+    assert_eq!(r(vec![Expr::Number(4)]), parse(&State::default(), "::"));
+    assert_eq!(r(vec![Expr::Number(5)]), parse(&State::default(), ".::"));
+    assert_eq!(r(vec![Expr::Number(6)]), parse(&State::default(), ":::"));
+    assert_eq!(r(vec![Expr::Number(7)]), parse(&State::default(), ".:::"));
 }
 
 #[test]
-fn parse_conditional_without_else() {
+fn parse_conditional() {
     assert_eq!(
-        r(Expr::Conditional {
+        r(vec![Expr::Conditional {
             condition: b(Expr::Number(1)),
             success: b(Expr::Number(1)),
-            failure: None,
-        }),
-        expr(&State::default(), "given that.{.")
+            failure: b(None),
+        }]),
+        parse(&State::default(), "given that..")
     );
 }
 
 #[test]
-fn parse_conditional_without_else_whitespace() {
+fn parse_conditional_whitespace() {
     assert_eq!(
-        r(Expr::Conditional {
+        r(vec![Expr::Conditional {
             condition: b(Expr::Number(1)),
             success: b(Expr::Number(1)),
-            failure: None,
-        }),
-        expr(&State::default(), "given that . { . ")
+            failure: b(None),
+        }]),
+        parse(&State::default(), "given that . . ")
     );
 }
 
 #[test]
 fn parse_conditional_with_else() {
     assert_eq!(
-        r(Expr::Conditional {
+        r(vec![Expr::Conditional {
             condition: b(Expr::Number(1)),
             success: b(Expr::Number(1)),
-            failure: Some(b(Expr::Number(1))),
-        }),
-        expr(&State::default(), "given that.{.otherwise.")
+            failure: b(Some(Expr::Number(1))),
+        }]),
+        parse(&State::default(), "given that..otherwise.")
     );
+}
+
+#[test]
+fn parse_conditional_with_else_whitespace() {
+    assert_eq!(
+        r(vec![Expr::Conditional {
+            condition: b(Expr::Number(1)),
+            success: b(Expr::Number(1)),
+            failure: b(Some(Expr::Number(1))),
+        }]),
+        parse(&State::default(), "given that . . otherwise . ")
+    );
+}
+
+#[test]
+fn parse_function_definition() {
+    assert_eq!(
+        r(vec![Expr::Definition(Ident("öäå".into()), b(Expr::Number(1)))]),
+        parse(&State::default(), "öäå¤.")
+    );
+}
+
+#[test]
+fn parse_function_definition_whitespace() {
+    assert_eq!(
+        r(vec![Expr::Definition(Ident("öäå".into()), b(Expr::Number(1)))]),
+        parse(&State::default(), "öäå ¤ .")
+    );
+}
+
+#[test]
+fn parse_function_definition_with_param() {
+    assert_eq!(
+        r(vec![Expr::Definition(Ident("ö".into()), b(Expr::Param(1)))]),
+        parse(&State::default(), r"ö ¤ \.")
+    );
+}
+
+#[test]
+fn parse_multiple_function_definitions() {
+    assert_eq!(
+        r(vec![
+            Expr::Definition(Ident("ö".into()), b(Expr::Param(1))),
+            Expr::Definition(Ident("ä".into()), b(Expr::Param(1))),
+        ]),
+        parse(&State::default(), r"ö ¤ \.
+        ä ¤ \.")
+    );
+}
+
+#[test]
+fn parse_preceding_whitespace() {
+    assert_eq!(
+        r(vec![Expr::Number(1)]),
+        parse(&State::default(), r"     .")
+    );
+}
+
+#[test]
+fn parse_scope() {
+    assert_eq!(
+        r(vec![Expr::Scope(b(Expr::Number(1)))]),
+        parse(&State::default(), r"{.)")
+    );
+}
+
+#[test]
+fn parse_scope_whitespace() {
+    assert_eq!(
+        r(vec![Expr::Scope(b(Expr::Number(1)))]),
+        parse(&State::default(), r"{ . ) ")
+    );
+}
+
+#[test]
+fn parse_scope_ending_to_eof() {
+    assert_eq!(
+        r(vec![Expr::Scope(b(Expr::Number(1)))]),
+        parse(&State::default(), r"{.")
+    );
+}
+
+#[test]
+fn parse_scope_ending_to_linebreak() {
+    assert_eq!(
+        r(vec![Expr::Scope(b(Expr::Number(1)))]),
+        parse(&State::default(), r"{.
+        ")
+    );
+}
+
+#[test]
+fn parse_addition() {
+    assert_eq!(
+        r(vec![Expr::Op(b(Expr::Number(1)), Op::Add, b(Expr::Number(1)))]),
+        parse(&State::default(), ".+.")
+    )
+}
+
+#[test]
+fn parse_addition_whitespace() {
+    assert_eq!(
+        r(vec![Expr::Op(b(Expr::Number(1)), Op::Add, b(Expr::Number(1)))]),
+        parse(&State::default(), ". + .")
+    )
+}
+
+#[test]
+fn parse_equality() {
+    assert_eq!(
+        r(vec![Expr::Op(b(Expr::Number(1)), Op::Equ, b(Expr::Number(1)))]),
+        parse(&State::default(), ".=.")
+    )
+}
+
+#[test]
+fn parse_equality_whitespace() {
+    assert_eq!(
+        r(vec![Expr::Op(b(Expr::Number(1)), Op::Equ, b(Expr::Number(1)))]),
+        parse(&State::default(), ". = .")
+    )
+}
+
+
+#[test]
+fn parse_realer_function() {
+    assert_eq!(
+        r(vec![Expr::Definition(Ident("ö".into()), b(
+            Expr::Op(
+                b(Expr::Scope(b(Expr::Op(b(Expr::Param(1)), Op::Add, b(Expr::Param(2)))))), 
+                Op::Mul,
+                b(Expr::Op(b(Expr::Param(1)), Op::Add, b(Expr::Number(7))))
+        )))]),
+        parse(&State::default(), r"ö ¤ {\. + \:) * \. + .:::")
+    )
+}
+
+#[test]
+fn parse_writing_io() {
+    assert_eq!(
+        r(vec![Expr::WriteIO(b(Expr::Number(1)))]),
+        parse(&State::default(), "@ << .")
+    )
+}
+
+#[test]
+fn parse_simple_text() {
+    assert_eq!(
+        r(vec![Expr::Text("simple".into())]),
+        parse(&State::default(), "/simple")
+    )
+}
+
+#[test]
+fn parse_space_text() {
+    assert_eq!(
+        r(vec![Expr::Text("  ".into())]),
+        parse(&State::default(), "/ / ")
+    )
+}
+
+#[test]
+fn parse_phrase_text() {
+    assert_eq!(
+        r(vec![Expr::Text("Hello, World!".into())]),
+        parse(&State::default(), "/Hello,/ World!")
+    )
+}
+
+#[test]
+fn parse_slash_text() {
+    assert_eq!(
+        r(vec![Expr::Text("/".into())]),
+        parse(&State::default(), "//")
+    )
+}
+
+#[test]
+fn parse_slash_space_text() {
+    assert_eq!(
+        r(vec![Expr::Text("/ ".into())]),
+        parse(&State::default(), "// ")
+    )
+}
+
+#[test]
+fn parse_space_slash_text() {
+    assert_eq!(
+        r(vec![Expr::Text(" /".into())]),
+        parse(&State::default(), "/ //")
+    )
+}
+
+#[test]
+fn parse_slashmiddle_text() {
+    assert_eq!(
+        r(vec![Expr::Text("eyey".into())]),
+        parse(&State::default(), "/ey/ey")
+    )
+}
+
+#[test]
+fn parse_slashmiddle_space_text() {
+    assert_eq!(
+        r(vec![Expr::Text("ey/ ey".into())]),
+        parse(&State::default(), "/ey// ey")
+    )
+}
+#[test]
+fn parse_text_ending_linebreak() {
+    assert_eq!(
+        r(vec![Expr::Text("aaa".into())]),
+        parse(&State::default(), "/aaa
+")
+    )
+}
+
+#[test]
+fn parse_printing_text_to_io() {
+    assert_eq!(
+        r(vec![Expr::WriteIO(b(Expr::Text("true".into())))]),
+        parse(&State::default(), "@ << /true ")
+    )
+}
+
+#[test]
+fn parse_call_function_in_scope() {
+        assert_eq!(
+        r(vec![Expr::Scope(b(Expr::Call(Ident("ö".into()), vec![Expr::Number(1), Expr::Number(3)])))]),
+        parse(&State::default(), "{ö . .:)")
+    )
+}
+
+#[test]
+fn parse_compare_equality_of_number_and_function_call() {
+    assert_eq!(
+        r(vec![Expr::Op(
+            b(Expr::Number(24)),
+            Op::Equ,
+            b(Expr::Scope(b(Expr::Call(Ident("ö".into()), vec![Expr::Number(1), Expr::Number(3)]))))
+        )]),
+        parse(&State::default(), ":::::::::::: = {ö . .:)")
+    )
+}
+
+#[test]
+fn parse_condition_that_prints() {
+    assert_eq!(
+        r(vec![
+            Expr::Conditional {
+                condition: b(Expr::Op(
+                    b(Expr::Number(2)),
+                    Op::Equ,
+                    b(Expr::Number(1)),
+                )),
+                success: b(
+                    Expr::WriteIO(b(Expr::Text("true".into())))
+                ),
+                failure: b(
+                    None
+                )
+            }
+        ]),
+        parse(&State::default(), r"given that : = . @ << /true")
+    )
+}
+
+#[test]
+fn parse_realer_expression() {
+    assert_eq!(
+        r(vec![
+            Expr::Conditional {
+                condition: b(Expr::Op(
+                    b(Expr::Number(24)),
+                    Op::Equ,
+                    b(Expr::Scope(b(Expr::Call(Ident("ö".into()), vec![Expr::Number(1), Expr::Number(3)]))))
+                )),
+                success: b(
+                    Expr::WriteIO(b(Expr::Text("true".into())))
+                ),
+                failure: b(
+                    Some(Expr::WriteIO(b(Expr::Text("false".into()))))
+                )
+            }
+        ]),
+        parse(&State::default(), r"given that :::::::::::: = {ö . .:) @ << /true otherwise @ << /false")
+    )
+}
+
+#[test]
+fn parse_example() {
+    assert_eq!(
+        r(vec![
+            Expr::Definition(Ident("ö".into()), b(
+                Expr::Op(
+                    b(Expr::Scope(b(Expr::Op(b(Expr::Param(1)), Op::Add, b(Expr::Param(2)))))), 
+                    Op::Mul,
+                    b(Expr::Op(b(Expr::Param(1)), Op::Add, b(Expr::Number(7))))
+            ))),
+            Expr::Conditional {
+                condition: b(Expr::Op(
+                    b(Expr::Number(24)),
+                    Op::Equ,
+                    b(Expr::Scope(b(Expr::Call(Ident("ö".into()), vec![Expr::Number(1), Expr::Number(3)]))))
+                )),
+                success: b(
+                    Expr::WriteIO(b(Expr::Text("true".into())))
+                ),
+                failure: b(
+                    Some(Expr::WriteIO(b(Expr::Text("false".into()))))
+                )
+            }
+        ]),
+        parse(&State::default(), r"
+ö ¤ {\. + \:) * \. + .:::
+
+given that :::::::::::: = {ö . .:) @ << /true otherwise @ << /false
+        ")
+    )
 }
