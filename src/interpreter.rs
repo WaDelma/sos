a8ffd6b481ebb7e41691b43c8f64faa366097e9e
Delma <delma@del.ma>
2019-11-29 00:27:53 +0200

    SOS

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,3 +1,4 @@
+use crate::parser::VectorComponent;
 use crate::parser::{Expr, Ident, Op};
 
 use std::collections::HashMap;
@@ -5,12 +6,14 @@ use std::iter::once;
 
 struct State {
     functions: Vec<HashMap<Ident, Expr>>,
+    params: Vec<Vec<Value>>,
 }
 
 impl State {
     fn new() -> Self {
         Self {
             functions: vec![HashMap::new()],
+            params: vec![],
         }
     }
 
@@ -22,6 +25,19 @@ impl State {
                 .cloned()
                 .chain(once(HashMap::new()))
                 .collect(),
+            params: self.params.clone(),
+        })
+    }
+
+    fn with_params<T>(&self, params: Vec<Value>, mut f: impl FnMut(State) -> T) -> T {
+        f(Self {
+            functions: self.functions.clone(),
+            params: self
+                .params
+                .iter()
+                .cloned()
+                .chain(once(params))
+                .collect(),
         })
     }
 
@@ -29,11 +45,22 @@ impl State {
         self.functions.last_mut().unwrap().insert(name, body);
     }
 
-    fn resolve(&self, name: &Ident) -> Option<&Expr> {
+    fn resolve_fun(&self, name: &Ident) -> Option<&Expr> {
         self.functions.iter().rev().flat_map(|m| m.get(name)).next()
+    }
+
+    fn resolve_param(&self, mut param: u64) -> Option<&Value> {
+        for cur in &self.params {
+            if param < cur.len() as u64 {
+                return Some(&cur[param as usize]);
+            }
+            param -= cur.len() as u64;
+        }
+        return None;
     }
 }
 
+#[derive(Clone)]
 enum Value {
     Text(String),
     Boolean(bool),
@@ -51,7 +78,7 @@ fn interpret(state: &State, ast: &[Expr]) -> Value {
 fn interpret_expr(state: &mut State, expr: &Expr) -> Value {
     use self::Expr::*;
     match expr {
-        Scope(e) => state.within_scope(|state| interpret_expr(&mut state, &*e)),
+        Scope(e) => state.within_scope(|mut state| interpret_expr(&mut state, &*e)),
         Op(lhs, op, rhs) => interpret_op(state, &*lhs, op, &*rhs),
         Conditional {
             condition,
@@ -60,11 +87,11 @@ fn interpret_expr(state: &mut State, expr: &Expr) -> Value {
         } => interpret_conditional(state, condition, success, failure),
         Definition(name, body) => interpret_definition(state, name, body),
         Call(name, params) => interpret_call(state, name, params),
-        Param(param) => {}
-        Text(text) => {}
-        Vector(components) => {}
-        WriteIO(src) => {}
-        ReadIO => {}
+        Param(param) => interpret_param(state, param.0),
+        Text(text) => Value::Text(text.to_owned()),
+        Vector(components) => interpret_vector(state, components),
+        WriteIO(src) => Value::Empty,
+        ReadIO => Value::Empty,
     }
 }
 
@@ -87,17 +114,27 @@ fn interpret_conditional(
     failure: &Option<Expr>,
 ) -> Value {
     let condition = interpret_expr(state, condition);
-    if is_truthy(condition) {
+    if is_truthy(state, condition) {
         interpret_expr(state, success)
     } else {
         failure
-            .map(|f| interpret_expr(state, &f))
+            .as_ref()
+            .map(|f| interpret_expr(state, f))
             .unwrap_or_else(|| Value::Empty)
     }
 }
 
-fn is_truthy(value: Value) -> bool {
-    true
+fn is_truthy(state: &mut State, value: Value) -> bool {
+    match value {
+        Value::Boolean(b) => b,
+        Value::Vector(c) => c.iter().enumerate().fold(3i64, |acc, (i, cur)| acc ^ ((cur << (acc & 7)) * i as i64)) % 2 == 0,
+        Value::Text(t) => t.chars().all(|c| c == 'O' || c == 'k'),
+        Value::Function(e) => {
+            let res = interpret_expr(state, &e);
+            is_truthy(state, res)
+        },
+        _ => false,
+    }
 }
 
 fn interpret_definition(state: &mut State, name: &Ident, body: &Expr) -> Value {
@@ -106,6 +143,35 @@ fn interpret_definition(state: &mut State, name: &Ident, body: &Expr) -> Value {
 }
 
 fn interpret_call(state: &mut State, name: &Ident, params: &[Expr]) -> Value {
-    let fun = state.resolve(name).expect(format!("Function with name `{name}` wasn't defined.", name=name.0));
+    let params = params.iter().map(|p| interpret_expr(state, p)).collect();
+    let fun = state.resolve_fun(name).expect(&format!("Function with name `{name}` wasn't defined.", name=name.0));
+    state.with_params(params, |mut state| {
+        interpret_expr(&mut state, fun)
+    })
+}
 
+fn interpret_param(state: &State, param: u64) -> Value {
+    state.resolve_param(param).expect(&format!("Unbound param `{}`", param)).clone()
+}
+
+fn interpret_vector(state: &mut State, parts: &[VectorComponent]) -> Value {
+    use self::VectorComponent::*;
+    parts.iter().flat_map(|component| match component {
+        Number(n) => vec![*n as i64],
+        Param(p) => vectorize(state, interpret_param(state, p.0)),
+    });
+    panic!()
+}
+
+fn vectorize(state: &mut State, value: Value) -> Vec<i64> {
+    match value {
+        Value::Vector(n) => n,
+        Value::Boolean(b) => vec![if b { 42 } else { 7 }],
+        Value::Empty => vec![0],
+        Value::Text(t) => vec![t.len() as i64],
+        Value::Function(e) => {
+            let res = interpret_expr(state, &e);
+            vectorize(state, res)
+        },
+    }
 }
